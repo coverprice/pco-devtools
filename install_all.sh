fd38a17ab9730c31288f75420c4974fad36eb459
James Russell <jrussell@redhat.com>
2023-09-19 23:07:56 -0700

    Minor renames

--- install_all.sh
+++ install_all.sh
@@ -4,6 +4,6 @@ set -o errexit -o nounset -o pipefail
 INSTALL_ALL_HERE="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 "${INSTALL_ALL_HERE}/install_editor_tools.sh"
-"${INSTALL_ALL_HERE}/install_tools.sh"
+"${INSTALL_ALL_HERE}/install_devtools.sh"
 "${INSTALL_ALL_HERE}/install_python_tools.sh"
 "${INSTALL_ALL_HERE}/install_navtools.sh"
